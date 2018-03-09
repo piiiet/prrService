4a517aa9b56841579dd4304ec69a5c571410f2a6
pleistner <leistner@traffics.de>
2018-03-09 11:18:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express       = require('express');
+const config        = require('./config/config');
+const conditions    = require('./routes/conditions');
+
+
+// application
+const app = express();
+
+// routes
+app.use('/conditions', conditions);
+
+
+app.listen(config.get('port'), function () {
+    console.log(`archiveService running on port ${config.get('port')}`);
+});
