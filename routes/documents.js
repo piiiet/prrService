304e5f8439ec7facf2ed4efeb0bd111b8418f850
pleistner <leistner@traffics.de>
2018-04-24 12:53:43 +0200

    fixed documents

--- routes/documents.js
+++ routes/documents.js
@@ -6,15 +6,27 @@ const router = express.Router();
 const ArchiveClient = require('../lib/ArchiveService/client');
 
 router.get('/:filename', function (req, res, next) {
-    ArchiveClient.get(req.params.filename, (error, response, body) => {}).pipe(res);
+    ArchiveClient.get(req.params.filename, (error, response, body) => {
+        if (error) {
+            return next(error);
+        }
+    }).pipe(res);
 });
 
 router.patch('/:filename', function (req, res, next) {
-    ArchiveClient.patch(req.params.filename, (error, response, body) => {}).pipe(res);
+    ArchiveClient.patch(req.params.filename, (error, response, body) => {
+        if (error) {
+            return next(error);
+        }
+    }).pipe(res);
 });
 
 router.delete('/:filename', function (req, res, next) {
-    ArchiveClient.delete(req.params.filename, (error, response, body) => {}).pipe(res);
+    ArchiveClient.delete(req.params.filename, (error, response, body) => {
+        if (error) {
+            return next(error);
+        }
+    }).pipe(res);
 });
 
 module.exports = router;
